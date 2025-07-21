7137beab51ccf98e2d5e0ea861c360dd9dd716c8
Griffin, Alexandra <griffial@goldbeck.biz>
2025-07-21 16:31:17 +0200

    Added component donut-card and added child components for the different donuts + scss styling

--- src/app/admin/containers/donut-list/donut-list.component.ts
+++ src/app/admin/containers/donut-list/donut-list.component.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Donut } from '../../models/donut.model';
+//It's a smart component
 @Component({
-  selector: 'app-donut-list',
-  template: `
-    <p>
-      donut-list works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: 'donut-list',
+  templateUrl: './donut-list.component.html',
+  styleUrls: ['./donut-list.component.scss'],
 })
+export class DonutListComponent implements OnInit {
+  donuts!: Donut[];
 
-export class DonutListComponent implements OnInit{
+  constructor() {}
 
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.donuts = [
+      {
+        id: '0001',
+        name: 'Just Chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        description: 'For the chocolate enthusiast',
+      },
+      {
+        id: '0002',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        description: 'For the fudgy self',
+      },
+      {
+        id: '0003',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 119,
+        description: 'Caramel orgasm',
+      },
+    ];
+  }
 }
